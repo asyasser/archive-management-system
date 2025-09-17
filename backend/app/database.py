4acc56b1f167ed4d987472e961259001a7a5eed0
asyasser <assouyasser1@gmail.com>
2025-09-17 22:23:55 +0100

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,22 +4,22 @@ from models import Base
 import os
 from dotenv import load_dotenv
 
-#load environment variables
+
 load_dotenv()
 
-#get database URL from environment
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-#create database engine
+
 engine = create_engine(DATABASE_URL)
 
-#create session factory
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#create all tables
+
 Base.metadata.create_all(bind=engine)
 
-#dependency to get database session
+
 def get_db():
     db = SessionLocal()
     try:
